7da74f8b543cc60a4a6277117e36b1331bcf744b
Higino Silva <higino.silva@FFPTN1103.local>
2017-10-10 18:17:30 +0100

    First draft of idea listing. Still need to complete with persistance behaviour

--- IdeaManager_FE/wwwroot/js/site.js
+++ IdeaManager_FE/wwwroot/js/site.js
@@ -1,1 +1,18 @@
 ﻿// Write your JavaScript code.
+
+function newIdea() {
+      if ($("input[name=ideaTitle]").val() === '' || $("input[name=ideaAuthor]").val() === '') {
+        alert("You must write something!");
+        return;
+      }
+
+    $('#ideaList').append("<div class='col-sm-4'><h2>"+ $("input[name=ideaTitle]").val()+ "</h2>" + 
+                    "<div class='card'>" +
+                        "<img src='../images/idea-light-bulb.svg' alt='nice Avatar' style='width:20%'>" + 
+                            "<div class='container'>" +
+                               "<h4><b>"+ $("input[name=ideaAuthor]").val() + "</b></h4>" +
+                               "<p>Interior Designer</p>" +
+                            "</div>" + 
+                    "</div>" +
+                 "</div>");
+}
